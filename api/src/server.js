befda21f628d9016ca64ee9b6e2d6035a8a57e02
jayazzuro <vogiahao2005@gmail.com>
2026-03-17 14:37:35 +0700

    add path to server

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine.js");
+const path = require("path");
 const code_sessions = require("./routes/codeSession.routes.js");
 const executions = require("./routes/executions.routes.js");
 
